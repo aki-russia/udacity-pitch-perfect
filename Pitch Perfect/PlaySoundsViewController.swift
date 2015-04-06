540b7839873e6495a14d5538660b6dc154ffdaaf
Dmitriy Kharchenko <akimoto@yandex.ru>
2015-04-07 02:22:41 +0700

    initial commit

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Dmitriy Kharchenko on 3/28/15.
+//  Copyright (c) 2015 Dmitriy Kharchenko. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
+    var receivedAudio:RecordedAudio!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true;
+
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
+
+    @IBAction func playAsSnail(sender: UIButton) {
+        
+        audioPlayer.rate = 0.5;
+        audioPlayer.play();
+    }
+    @IBAction func playAsRabbit(sender: UIButton) {
+        
+        audioPlayer.rate = 2.0;
+        audioPlayer.play();
+        
+    }
+
+    @IBAction func playAsRat(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    @IBAction func playAsDevil(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    @IBAction func stopAudioPlayer(sender: UIButton) {
+        audioPlayer.stop();
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Dmitriy Kharchenko on 3/30/15.
+//  Copyright (c) 2015 Dmitriy Kharchenko. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
